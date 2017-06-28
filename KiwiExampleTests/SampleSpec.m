1262c041495b48b840c8ac1a77b1f1f14e7e46de
Elangbam <jelangba@MGC000000727>
2017-06-28 17:28:49 -0400

    Add test cases

--- KiwiExampleTests/SampleSpec.m
+++ KiwiExampleTests/SampleSpec.m
@@ -8,16 +8,12 @@
 
 
 #import "Kiwi.h"
+#import "ViewController.h"
+
 
 SPEC_BEGIN(MathSpec)
 
-//describe(@"Math", ^{
-//    it(@"is pretty cool", ^{
-//        NSUInteger a = 16;
-//        NSUInteger b = 26;
-//        [[theValue(a + b) should] equal:theValue(42)];
-//    });
-//});
+__block ViewController *vc = [[ViewController alloc]init];
 
 describe(@"My first Kiwi test", ^{
     context(@"A sample string", ^{
@@ -28,6 +24,38 @@ describe(@"My first Kiwi test", ^{
         });
         it(@"should be 'Hello, Universe'.", ^{
             [[greeting should]equal:@"Hello, Universe!"];
+        });
+    });
+});
+
+describe(@"Check a string for Palindrome", ^{
+    
+    
+    context(@"When the string is a Palindrome", ^{
+        BOOL isPalindrome = [vc isPalindrome:@"aba"];
+        it(@"shouls return true", ^{
+            [[theValue(isPalindrome) should] equal:theValue(YES)];
+            
+            
+        });
+    });
+    context(@"When the string is not Palindrome", ^{
+        BOOL isPalindrome = [vc isPalindrome:@"abc"];
+        it(@"Shouuld return false", ^{
+             [[theValue(isPalindrome) should] equal:theValue(NO)];
+            
+        });
+    });
+    
+});
+
+
+describe(@"Reverse a string", ^{
+    context(@"A string is given", ^{
+        NSString *strRev = [vc reverseThisString:@"abcd"];
+        
+        it(@"should return the reverse", ^{
+            [[strRev should]matchPattern:@"dcba"];
         });
     });
 });
